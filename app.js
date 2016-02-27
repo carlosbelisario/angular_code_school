ddda7a5f25ee0275881da4601b2fddf977afc4be
Carlos Belisario <carlos.belisario.gonzalez@gmail.com>
2016-02-27 14:53:06 -0430

    structure of gem

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-/**
- * angular appication
- * @type {angular.Module}
- */
-var app = angular.module('store', []);
+(function() {
+    /**
+     * angular appication
+     * @type {angular.Module}
+     */
+    var app = angular.module('store', []);
+
+    app.controller('StoreController', function() {
+        this.product = gem;
+    });
+
+    var gem = {
+        "name": "Decoratiom",
+        "price": 12.9,
+        "description": ". . ."
+    }
+
+})();
