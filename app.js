d327f9080e2f5f45ce2e9c7facdd2ad354e8a6ba
Carlos Belisario <carlos.belisario.gonzalez@gmail.com>
2016-02-27 15:27:37 -0430

    video 3 Built-in Directives nuevas expresiones y directivas

--- app.js
+++ app.js
@@ -6,21 +6,32 @@
     var app = angular.module('store', []);
 
     /**
-     * 
+     *
      * StoreController
      */
     app.controller('StoreController', function() {
-        this.product = gem;
+        this.products = gems;
     });
 
     /**
      *
-     * @type {{name: string, price: number, description: string}}
+     * @type {{name: string, price: number, description: string, cansPruchase: boolean}}
      */
-    var gem = {
-        "name": "Decoratiom",
-        "price": 12.9,
-        "description": ". . ."
-    }
+    var gems = [
+        {
+            "name": "Decoratiom",
+            "price": 12.9,
+            "description": ". . .",
+            "canPruchase": true,
+            "soldOut": false
+        },
+        {
+            "name": "Pentagonal Gem",
+            "price": 5.98,
+            "description": ". . .",
+            "canPruchase": false,
+            "soldOut": false
+        }
+    ];
 
 })();
